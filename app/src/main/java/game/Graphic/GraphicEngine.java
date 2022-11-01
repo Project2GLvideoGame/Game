0b6e48e56d035320502c1613b2e7cee4bb62ade8
Gazaix <damien.gazaix@etu.univ-amu.fr>
2022-11-01 10:48:16 +0100

    Ajout de addChildren dans graphic ainsi qu'un bouton de test

--- app/src/main/java/game/Graphic/GraphicEngine.java
+++ app/src/main/java/game/Graphic/GraphicEngine.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import game.GameObject;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class GraphicEngine extends Application {
+    
+    Group mainGroup = new Group();
 
     private final int width = 700;
     private final int height = 700;
@@ -19,18 +23,27 @@ public class GraphicEngine extends Application {
     List<Displayable> displayables = new ArrayList<>();
     List<Scene> scenes = new ArrayList<>();
 
-    private Parent createContent() {
-
+    //Initialise les gameObject Initialement présent dans la scène 
+    private void createContent() {
         GameObject g = new GameObject(new Displayable(new ImageView("shrek.jpeg"), 150, 150, 100, 100));
-        Group group = new Group();
-        group.getChildren().add(g.getComponent(Displayable.class).getAsset());
-        
-        return group;
+        Button btn = new Button("TestButton");
+        btn.setOnAction(this::actionEvent);
+
+        mainGroup.getChildren().add(g.getComponent(Displayable.class).getAsset());
+        displayables.add(g.getComponent(Displayable.class));
+
+        mainGroup.getChildren().add(btn);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent(), width, height));
+        createContent();
+        stage.setScene(new Scene(mainGroup, width, height));
         stage.show();
+    }
+
+    private void actionEvent(ActionEvent event){
+        scale(displayables.get(0), displayables.get(0).getAsset().getScaleX() + 0.2d);
     }
 
     public void setPosition(Displayable displayable, double x, double y) {
@@ -49,5 +62,10 @@ public class GraphicEngine extends Application {
 
     public void setVisibility(Displayable displayable, boolean value) {
         displayable.getAsset().setVisible(value);
+    }
+
+    public void addChildren(Displayable displayable){
+        mainGroup.getChildren().add(displayable.getAsset());
+        displayables.add(displayable);
     }
 }
