52e1c4936d7258b0f90c38a97d3fb48fd00acdc2
Gazaix <damien.gazaix@etu.univ-amu.fr>
2022-11-30 17:47:25 +0100

    add image de PV et ajout de la classe PlayerLifePoint

---
+++ app/src/main/java/game/entity/PlayerLifePoint.java
@@ -0,0 +1,16 @@
+package game.entity;
+
+import engine.GameObject;
+import engine.graphic.Displayable;
+
+public class PlayerLifePoint extends GameObject{
+    public PlayerLifePoint(){
+        super(new Displayable(0, 0, 100, 25, "/player/lifePoint/LifePoint3.png"));
+    }
+
+    public void graphicPlayerLife(int lifePoint){
+        if(lifePoint > 3) lifePoint = 3;
+        if(lifePoint < 0) lifePoint = 0;
+        getComponent(Displayable.class).setAsset("/player/lifePoint/LifePoint"+lifePoint+".png");
+    }
+}
